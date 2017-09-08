84f180b9e205257d3bd1c7686f561f15cb22208f
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2017-09-08 14:46:43 +0200

    Update device.py

--- Semester3/Dialogue_system2/lab1/device.py
+++ Semester3/Dialogue_system2/lab1/device.py
@@ -31,4 +31,37 @@ class HelloWorldDevice(DddDevice):
 			self._alarm_is_set = True
             return True
 
+
+
+
+## this is original
+
+from tdm.lib.device import DddDevice, DeviceWHQuery, DeviceAction
+
+
+class HelloWorldDevice(DddDevice):
+    def __init__(self):
+        self.reset()
+        
+    def reset(self):
+        self._time_hour = 00
+        self._time_minute = 00
+        #       self._alarm_hour = 00
+        #       self._alarm_minute = 00
+        #       self._alarm_is_set = False
+        
+    class current_time(DeviceWHQuery):
+        def perform(self):
+            time = '"%02d:%02d"' % (self.device._time_hour, self.device._time_minute)
+            return [time]
+    
+    class SetTime(DeviceAction):
+        PARAMETERS = ["hour_to_set", "minute_to_set"]
+        def perform(self, hour, minute):
+            self.device._time_hour = hour
+            self.device._time_minute = minute
+            return True
+            
+    # class SetAlarm(DeviceAction): COMPLETE THIS CLASS
+
 	
