13913c59bfbe4d5ae2d6e7a20962b06889e3e5fe
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2017-09-09 07:25:37 -1100

    Update feature.py

--- Semester3/Machine_learning/lab1/feature.py
+++ Semester3/Machine_learning/lab1/feature.py
@@ -2,8 +2,8 @@
 
 #========================== import Module ==========================#
 
-
-from collections import Counter
+import numpy as np
+import collections
 from collections import defaultdict
 
 #import nltk
@@ -24,23 +24,31 @@ T = 1#len(tag_counter)
 V = 1#len(word_counter)
 A = 0.01
 unseen_word_dict = {}
-word_scale_book_dict = defaultdict(list)
+author_word_dictionary = defaultdict(list)
+word_in_sent = []           # defaultdict(list) #looks like, 
 #defaultdict is not dict, cannot be re-called.
-attr_num = 1
 
+track_attr_num = 1
 stops = set(stopwords.words('english'))
 punctuation = ['.', ',', '\/', '\'', '"', '?', '!', '@', '#', '$', '-', '--', '...', '[', ']', '{', '}', ':', ';', '/', ',"', '."', "!\""]
 
 #========================== Constant ==========================#
 
 
+#  annotated_sents = [('a', "This is glory"), ('b', "sent3"), ...]
+def tokenize(annotated_sents):
+    global word_in_sent
+    global author_word_dictionary
 
-def tokenize(use_book_dict_here):
-    global word_scale_book_dict
+    for author, sent in annotated_sents:
+        tokenized_sent = sent.rstrip().split(' ')
+        word_in_sent.append((author, tokenized_sent))
+        #word_in_sent : [(author, [word1, word2, word3...])]
 
-    for each_book_name in use_book_dict_here:
-        for sent in use_book_dict_here[each_book_name]:
-            word_scale_book_dict[each_book_name] = sent.split(' ')
+        for word in tokenized_sent:
+            author_word_dictionary[author].append(word)
+        #author_word_dictionary : {'author1' : [word1, word2, word3,...])]
+
     return None
 
 def punct_number(sent):
@@ -49,62 +57,152 @@ def punct_number(sent):
         if word in punctuation: sum+=1
     return sum
 
+#author_word_dictionary : {'author1' : [word1, word2, word3,...])]
+def remove_punct(author_word_dictionary):
+    punt_removed_dict = defaultdict(list)
+    for author in author_word_dictionary:
+        for word in author_word_dictionary[author]:
+            if word in punctuation : pass
+            else : punt_removed_dict[author].append(word)
+    return punt_removed_dict
 
-
+def remove_stopword(author_word_dictionary):
+    stop_removed_dict = defaultdict(list)
+    for author in author_word_dictionary:
+        for word in author_word_dictionary[author]:
+            if word in stops : pass
+            else : stop_removed_dict[author].append(word)
+    return stop_removed_dict
 
 #########################################################################################
                       # START     Getting attributes       START #
 #########################################################################################
 
-def feature1(sent_scale_dict_here):
+#  annotated_sents = [('a', "This is glory"), ('b', "sent3"), ...]
+def feature1(annotated_sents):
     """ punct_sentence_ratio  : len(sent)/#_of_punctuation"""
 
-    feature1_dict = defaultdict(list)
-    for each_book_name in sent_scale_dict_here:
-        for sent in sent_scale_dict_here[each_book_name]:
-            print(len(sent), sent,punct_number(sent))
-            feature1_dict[each_book_name] = 50*len(sent)/(punct_number(sent)+0.00000001)
-    print(feature1_dict)
-    return feature1_dict
+    feature1_list = []
+    for author, sent in annotated_sents:
+            #print(len(sent), sent, punct_number(sent))
+            feature1_list.append( (author, float("{0:.2f}".format(np.exp(len(sent)/10)/(punct_number(sent)+0.00001)))))
+    #print("THis is feature1: ",feature1_list)
+ 
+    return feature1_list
 
-def feature2(word_scale_book_dict):
+#word_in_sent : [(author, [word1, word2, word3...]), (aut2, [w1, w2...])]
+
+def feature2(annotated_sents):
     """ # of long words"""
 
-    feature2_dict = defaultdict(list)
-    for each_book_name in word_scale_book_dict:
+    tokenize(annotated_sents)
+    feature2_list = []
+
+    for author, word_list in word_in_sent:
         score = 0
-        for word in word_scale_book_dict[each_book_name]:
-            print(word, len(word))
+        for word in word_list:
+            #print(word, len(word))
             if len(word) >= 4 : score+=1
-        print("score is {0}".format(score))
-        feature2_dict[each_book_name] = score
+        #print("score is {0}".format(score))
+        feature2_list.append((author, score))
 
-    return feature2_dict
+    #print("THis is feature2: ", feature2_list)
 
+    return feature2_list
 
-def feature3():
-    return None
+#author_word_dictionary : {'author1' : [word1, word2, word3,...])]
+def feature3(annotated_sents):
+    feature3_list = []
+    for author, sent in annotated_sents:
+        feature3_list.append((author, 9999))
+    
+    counter=collections.Counter(author_word_dictionary['austen-persuasion.txt'])
+    print(counter.most_common(50))
+    counter=collections.Counter(author_word_dictionary['milton-paradise.txt'])
+    print(counter.most_common(50))
+    
+    punt_removed_dict = remove_punct(author_word_dictionary)
+    counter=collections.Counter(punt_removed_dict['austen-persuasion.txt'])
+    print(counter.most_common(50))
+    counter=collections.Counter(punt_removed_dict['milton-paradise.txt'])
+    print(counter.most_common(50))
+
+    #print(author_word_dictionary['austen-persuasion.txt'][:40])
+    return feature3_list
 
 
 class attributes_collection():
     """
     collections of authors + attributes
     """
-    def __init__(self):     # e.g., attributes_collection(feature1)
+    def __init__(self, args_obj, annotated_sents):     # e.g., attributes_collection(feature1)
+        self.args    = args_obj
+        self.svd     = args_obj.svd         # 200
+        self.novocab = args_obj.novocab # True/False
+        self.sents   = annotated_sents
+        #feature_list : [f1, f2...]
+        self.final_list   = []
+
+        self.features = ['punct_sentence_ratio',
+                          "#_of_long_words",
+                          'Test_attribute3']
+
+    #sent1 : [aut1, [13, 5133, 0.11]]
+    #sent2 : [aut2, [15, 2929, 0.02]]
+
+    #Total : [  [aut1, [13, 5133, 0.11]],
+    #           [aut2, [15, 2929, 0.02]] ......]
+    def extract_attr(self, svd=0, novocab=False): # [('aut1', 3), ('aut2', 1)...]
         
-        self.author_name = {}
-        
-    #{author1 : {attribute1 : [142, 252, 234], attribute2 : [1,2,2]}
-    def add(self, input_feature_here):
-        global attr_num
-        for author in input_feature_here:
-            if attr_num == 1 :
-                self.author_name[author] = {}
-                self.author_name[author]['attribute%d'%(attr_num)] = input_feature_here[author]
-            else :
-                self.author_name[author]['attribute%d'%(attr_num)] = input_feature_here[author]
-        attr_num +=1
-        print(attr_num, '\n')
+        for i in range(len(self.features)):
+            each_feature_list = eval("feature"+str(i+1))(self.sents)
+
+            #print(each_feature_list[:10])
+
+            if self.final_list ==[]:
+                for author, value in each_feature_list:
+                    temp = []
+                    temp.append(author)
+                    temp.append([value]) # ['author', [22]]
+                    # print(temp[0], "=-----stop=======",author)
+                    # print(temp[0]==author)
+                    self.final_list.append(temp)  #[[milton, [0.14]], [austen, [0.05]]]
+
+            else:
+                #print('ooooooooooooooooooooooooooooooooooooooo')
+                flag = 0 #total len(each_feature_list) = 4475
+                for author, value in each_feature_list:
+                    self.final_list[flag][1].append(value)  # ['author', [22, 1.492]]
+                        #print(len(self.final_list), '------\n', len(each_feature_list), each_feature_list[:10])
+                        #print("===========", self.final_list[i][0], author)
+                        #assert(self.final_list[i][0]==author)
+                    #print('flaggggggg', self.final_list[flag][0], author, self.final_list[flag])
+                    flag+=1
+                 # [  ['author1', [22, 1]], ['author2', [10, 2]] ]
+            print("one_round successful")
+
+        #print(attr_num, '\n')
+        return(print("all successful"))
+
+    def save_to_arff(self):
+        with open('sungmin_test.txt', 'w') as f:
+            f.write("@RELATION {}\n\n".format("text_classifier"))
+            for attr_name in self.features:
+                f.write("@ATTRIBUTE {} REAL\n".format(attr_name))
+
+            temp = "@ATTRIBUTE class {"
+            for class_name in self.args.files:
+                temp+=(class_name+',')
+            
+            f.write(temp[:-1]+"}\n\n@DATA\n")
+
+            for line in self.final_list: # line = [austen, [22, 3]]
+                temp = ""
+                for score in line[1]:
+                    temp += str(score)+","
+                temp+=str(line[0])+"\n"
+                f.write(temp)
+        return(print("arff save successful"))
 
     def __str__(self):
         return self.__name__
@@ -117,16 +215,14 @@ class attributes_collection():
 #==========================      RUNNING    ==========================#
 
 if __name__ == "__main__":
-    book_dict = {'a' : ['a b c , . aasd basdasdws abbbb a b a a a,'], 'b' : ['gsswa q we r : ; ,, e']}
-    tokenize(book_dict) # save to "word_scale_book_dict"
-    feature1 = feature1(book_dict)
-    feature2 = feature2(word_scale_book_dict)
-    test = attributes_collection()
-    test.add(feature1)
-    test.add(feature2)
-    print(vars(test), dir(test))
+    #annotated_sents = [('aut1', 'a b c , . aasd basdasdws abbbb a b a a a,'), ('aut2', 'gsswa q we r : ; ,, e')]
+    #annotated_sents = [('a', "sent11"), ('b', "sent3"), ...]
+
+    test = attributes_collection(args)
+    test.extract_attr()
+    #print(vars(test), dir(test))
     
-    #output : {'author_name':{
+    #output : {'final_list':{
     #           'b': {'attribute2': 1, 'attribute1': 212.49999946875}, 
     #           'a': {'attribute2': 3, 'attribute1': 433.3333318888889}}}
 
