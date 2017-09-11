9ff90ac02a6c9d5f9dfe8ec18a174cefbbfbd6cf
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2017-09-11 17:44:23 +0200

    Update device.py

--- Semester3/Dialogue_system2/lab1/device.py
+++ Semester3/Dialogue_system2/lab1/device.py
@@ -13,34 +13,54 @@ class HelloWorldDevice(DddDevice):
         self._alarm_minute = 00
         self._alarm_is_set = False
 
-class current_time(DeviceWHQuery):
-    def perform(self):
-        time = '"%02d:%02d"' % (self.device._time_hour, self.device._time_minute)
-        return [time]
+    class current_time(DeviceWHQuery):
+        def perform(self):
+            time = '"%02d:%02d"' % (self.device._time_hour, self.device._time_minute)
+            return [time]
 
-class alarm_time(DeviceWHQuery):
-    def perform(self):
-        time = '"%02d:%02d"' % (self.device._time_hour, self.device._time_minute)
-        return [time]
+    class alarm_time(DeviceWHQuery):
+        def perform(self):
+            time = '"%02d:%02d"' % (self.device._alarm_hour, self.device._alarm_minute)
+            return [time]
 
-class SetTime(DeviceAction):
-    PARAMETERS = ["hour_to_set", "minute_to_set"]
-    def perform(self, hour, minute):
-        self.device._time_hour = hour
-        self.device._time_minute = minute
-        return True
-    def oneshot(self, hour, minute):
-        self.device._time_hour = hour
-        self.device._time_minute = minute
-        return True
 
-class SetAlarm(DeviceAction):
-    PARAMETERS = ["hour_to_set", "minute_to_set"]
-    def perform(self, hour, minute):
-        self.device._time_hour = hour
-        self.device._time_minute = minute
-        return True
-    def oneshot(self, hour, minute):    # it becomees   goal type="oneshot" in domain.xml
-        self.device._time_hour = hour
-        self.device._time_minute = minute
-        return True
+    class current_time_oneshot(DeviceWHQuery):
+        def perform(self):
+            time = '"%02d:%02d"' % (self.device._time_hour, self.device._time_minute)
+            return [time]
+
+    class alarm_time_oneshot(DeviceWHQuery):
+        def perform(self):
+            time = '"%02d:%02d"' % (self.device._alarm_hour, self.device._alarm_minute)
+            return [time]
+
+
+    class SetTime(DeviceAction):
+        PARAMETERS = ["hour_to_set", "minute_to_set"]
+        def perform(self, hour, minute):# I cannot define name of function. already taken by system.e.g., look inside of grammar_eng.xml, action name 'top' is already used in action. without any 'perform' definition
+            self.device._time_hour = hour
+            self.device._time_minute = minute
+            return True
+
+    class SetAlarm(DeviceAction):
+        PARAMETERS = ["hour_to_set", "minute_to_set"]
+        def perform(self, hour, minute):    # it becomees   goal type="oneshot" in domain.xml
+            self.device._alarm_hour = hour
+            self.device._alarm_minute = minute
+            self.device._alarm_is_set = True
+            return True
+
+    class SetTimeOneshot(DeviceAction):
+        PARAMETERS = ["hour_to_set", "minute_to_set"]
+        def perform(self, hour, minute):# I cannot define name of function. already taken by system.e.g., look inside of grammar_eng.xml, action name 'top' is already used in action. without any 'perform' definition
+            self.device._time_hour = hour
+            self.device._time_minute = minute
+            return True
+
+    class SetAlarmOneshot(DeviceAction):
+        PARAMETERS = ["hour_to_set", "minute_to_set"]
+        def perform(self, hour, minute):    # it becomees   goal type="oneshot" in domain.xml
+            self.device._alarm_hour = hour
+            self.device._alarm_minute = minute
+            self.device._alarm_is_set = True
+            return True
