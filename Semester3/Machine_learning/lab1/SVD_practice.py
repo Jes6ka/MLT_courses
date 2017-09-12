737ac73b20a53b4cc8bc1afb7d76cda4f634d9a1
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2017-09-12 13:51:27 +0200

    Create SVD_practice.py

---
+++ Semester3/Machine_learning/lab1/SVD_practice.py
@@ -0,0 +1,18 @@
+#get singular value
+from scipy.linalg import svdvals
+from scipy.linalg import svd
+
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+svdvals(a)
+#array([  1.68481034e+01,   1.06836951e+00,   4.41842475e-16])
+
+u,s,v = linalg.svd(a)
+origin_a = np.dot(v.transpose(), np.dot(np.diag(s**1), u.transpose())).transpose()
+
+S = np.diag(s)
+#array([[  1.68481034e+01,   0.00000000e+00,   0.00000000e+00],
+#       [  0.00000000e+00,   1.06836951e+00,   0.00000000e+00],
+#       [  0.00000000e+00,   0.00000000e+00,   4.41842475e-16]])
+
+#s[0] is most informative? value
