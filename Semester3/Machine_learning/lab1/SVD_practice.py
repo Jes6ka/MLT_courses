817bc1f256f1ac6c1c1be00decaf6a5d8e426757
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2017-09-13 11:18:15 +0200

    Update SVD_practice.py

--- Semester3/Machine_learning/lab1/SVD_practice.py
+++ Semester3/Machine_learning/lab1/SVD_practice.py
@@ -21,3 +21,36 @@ S = np.diag(s)
 #obtaining original matrix is important, or how to do dot. product to get useful information.
 #reference : https://stackoverflow.com/questions/31251689/how-to-invert-numpy-matrices-using-singular-value-decomposition
 #https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.linalg.svd.html
+
+
+#file = austen-sdkf.txt
+
+def flatted(any_list):
+	return [a for b in any_list for a in b]
+
+def make_sent_voca_matrix(file):
+	whole_voca = []
+	sent_voca_list  = []
+	num_sent   = 0
+
+	
+
+	with open(file) as f:
+		for sent in f:
+			whole_voca.appned(sent.split(' '))
+			sent_voca_list.append(flatted(sent.split(' ')))
+			num_sent+=1
+
+	whole_voca = set(flatted(whole_voca))
+
+	final = []
+
+	
+	for voca_in_sent in sent_voca_list:
+		if voca_in_sent in whole_voca:
+
+
+	return(sent_voca_matrix)
+
+
+	
