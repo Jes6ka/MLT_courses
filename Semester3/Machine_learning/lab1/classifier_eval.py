afd3cd79fe08be2c973950b67385be7f3b7e1f1d
SUNG MIN YANG 양 성 민 <sungmin.nlp@gmail.com>
2017-09-18 11:10:39 -1100

    Update classifier_eval.py

--- Semester3/Machine_learning/lab1/classifier_eval.py
+++ Semester3/Machine_learning/lab1/classifier_eval.py
@@ -10,7 +10,7 @@ from draw_tree_v2 import *
 
 import pickle
 from sklearn.metrics import confusion_matrix #http://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
-
+# from pandas_ml import ConfusionMatrix
 #========================== import Module ==========================#
 
 
@@ -30,7 +30,7 @@ args = parser.parse_args()
 #========================== Constant ==========================#
 REMOVE_CALSS = 1
 stamp = 0
-
+predicted_class = list()
 #========================== Constant ==========================#
 
 
@@ -49,35 +49,66 @@ def read_model(train_model):
 	    node_collection, node_relation_list = data
 	return node_collection, node_relation_list
 
-def classify(raw_data, nd, nr, mother_dict = dict(), order=0):
+def predict(one_line, node_relation_list, node_collection, flag=0):
+	global 	predicted_class
+	parent, child, attr, sp = node_relation_list[flag]
 
-	left, right = list(), list()
-	parent, child, attr, sp = nr[order]
+	if node_collection[flag][1] == "terminal" :
+		#print("reached the terminal\n", node_collection[flag][0][0], '\n\n')
+		predicted_class.append(node_collection[flag][0][0])
+		return node_collection[flag][0][0]
 
-	if mother_dict['groups'] != {}:
-		left, right = node['groups']
-	del(node['groups'])
 
+	if one_line[int(attr[-1])-1] <= sp:	#if it's small
+		#print("-----flag@@-----",flag, parent, child, attr, sp)
+		predict(one_line, node_relation_list, node_collection, flag=child)
+	else :							#if it's big
+		#print("-----flag-----",flag, parent, child, attr, sp)
+		for parent_, child_, attr_, sp_ in node_relation_list[flag+1:]:
+			if parent == parent_ :
+				#print("-----flag-$$$---",flag, parent_, child_, attr_, sp_, len(node_relation_list))
+				if child_ == len(node_relation_list) :
+					predicted_class.append(node_collection[child_][0][0])
+					pass
+				else : predict(one_line, node_relation_list, node_collection, flag=child_)
+
+def classifer(raw_data, node_relation_list, node_collection):
+	global 	predicted_class
 	for line in raw_data:
-		if line[int(attr[-1])-1] <= sp :left.append(line)
-		else :							 right.append(line)
-		
-			if parent=child-1 : classify(left, order=order+1)
-			else: 				classify(right,order=order+1 )
+		predict(line, node_relation_list, node_collection)
+		#print("succeful", len(predicted_class), predicted_class)
+	return None
 
-	return {'groups': (left, right)}
+def evaluation(raw_data, predicted_class):
+	y_true = [l[-1] for l in raw_data]
+	y_pred = predicted_class
+	print("true, pred lengths are ",len(y_true), len(y_pred))
+	conf_matrx= confusion_matrix(y_true, y_pred)
+	#tn, fp, fn, tp = conf_matrx.ravel()
+	# conf_matrx = ConfusionMatrix(y_true, y_pred)
+	# conf_matrx.print_stats()
+	print(conf_matrx)
+	#print("total precision : " (tn+tp)/(tn+tp+fn+fp))
+	return None
 
-def predict(one_line, node_relation_list):
-	for parent, child, attr, sp in node_relation_list:
-		node_relation_list.pop((parent, child, attr, sp))
-		if one_line[int(attr[-1])-1] <= sp:
-			predict(one_line, node_relation_list)
-		else :
-			predict(one_line, node_relation_list)
-			
-	return predict_class
 
 if __name__ == "__main__":
 	data = read_arff(args.test)
 	node_collection, node_relation_list = read_model(args.train_model)
 	print(node_collection, '\n\n', node_relation_list)
+	classifer(data, node_relation_list, node_collection)
+	print('heyhey \n',predicted_class[:20])
+	conf_matrx = evaluation(data, predicted_class)
+
+
+
+# >>> def t(a,b,c,d):
+# 	return (a+d)/(a+b+d+c)
+# >>> t(527,222,206,164)
+# 0.6175156389633601
+# >>> t(616,133,231,139)
+# 0.6747095621090259
+# >>> t(666,83,320,50)
+# 0.6398570151921358
+
+#python classifer_eval.py exp2_test.arff exp1_train.model
